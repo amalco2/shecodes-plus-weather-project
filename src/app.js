ccfaaeb63cfe26828998f25cab9eea2f5fc4bd7b
Sasha Malcolmson <64798484+amalco2@users.noreply.github.com>
2023-06-23 18:56:33 -0400

    Removed precipitation

--- src/app.js
+++ src/app.js
@@ -14,12 +14,11 @@ return `${day} ${hours}:${minutes}`
 }
 
 function displayTemperature(response) {
-
+console.log(response.data)
   
 let temperatureElement = document.querySelector("#temperature");
 let cityElement = document.querySelector("#city");
 let descriptionElement = document.querySelector("#description");
-
 let humidityElement = document.querySelector("#humidity");
 let windElement = document.querySelector("#wind");
 let dateElement = document.querySelector("#date");
@@ -27,16 +26,16 @@ let dateElement = document.querySelector("#date");
 temperatureElement.innerHTML = Math.round(response.data.temperature.current);
 cityElement.innerHTML = (response.data.city);
 descriptionElement.innerHTML = (response.data.condition.description);
-
 humidityElement.innerHTML = (response.data.temperature.humidity);
 windElement.innerHTML = Math.round(response.data.wind.speed);
 dateElement.innerHTML = formatDate(response.data.time*1000);
 }
 
 let apiKey = "040c3166574e3dftbd2oda29cab8383c";
+let city = "Barcelona"
 let units = "metric";
 
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Columbia&key=${apiKey}&units=${units}`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
 
 
 axios.get(apiUrl).then(displayTemperature);
