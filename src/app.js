57302cd804dbe074fce86c9c0acc1f59a8a0e28b
Sasha Malcolmson <64798484+amalco2@users.noreply.github.com>
2023-06-23 18:48:22 -0400

    Added formatDate with date and time

--- src/app.js
+++ src/app.js
@@ -1,6 +1,20 @@
+function formatDate(timestamp) {
+let date = new Date(timestamp);
+let hours = date.getHours();
+if (hours < 10) {
+    hours = `0${hours}`;
+}
+let minutes = date.getMinutes();
+if (minutes < 10) {
+    minutes = `0${minutes}`;
+}
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let day = days[date.getDay()];
+return `${day} ${hours}:${minutes}`
+}
+
 function displayTemperature(response) {
 
-    console.log(response.data)
   
 let temperatureElement = document.querySelector("#temperature");
 let cityElement = document.querySelector("#city");
@@ -8,6 +22,7 @@ let descriptionElement = document.querySelector("#description");
 
 let humidityElement = document.querySelector("#humidity");
 let windElement = document.querySelector("#wind");
+let dateElement = document.querySelector("#date");
 
 temperatureElement.innerHTML = Math.round(response.data.temperature.current);
 cityElement.innerHTML = (response.data.city);
@@ -15,6 +30,7 @@ descriptionElement.innerHTML = (response.data.condition.description);
 
 humidityElement.innerHTML = (response.data.temperature.humidity);
 windElement.innerHTML = Math.round(response.data.wind.speed);
+dateElement.innerHTML = formatDate(response.data.time*1000);
 }
 
 let apiKey = "040c3166574e3dftbd2oda29cab8383c";
